f7f0c6e95e5eea181e1de004d4ec5754d4c0b4e4
Pavan Sudarshan <itspanzi@gmail.com>
2012-08-27 14:25:20 +0530

    Added the script that gets the latest job number and waits till that is complete. Once its done, the script fails if the job was a failure or it passes

---
+++ manage_jenkins_job.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+JENKINS_HOST=localhost
+JENKINS_PORT=10000
+JENKINS_JOB=website-build
+
+function latest_job {
+    JENKINS_LATEST_JOB=`curl -s "http://$JENKINS_HOST:$JENKINS_PORT/job/$JENKINS_JOB/api/xml?tree=lastBuild\[number\]&xpath=//number/text()"`
+}
+
+function jenkins_job_instance {
+    curl -s "http://$JENKINS_HOST:$JENKINS_PORT/job/$JENKINS_JOB/$1/api/xml?tree=$2&xpath=$3"
+}
+
+function is_building {
+    is_building=`jenkins_job_instance $1 "building" "//building/text()"`
+}
+
+function wait_to_complete {
+    is_building $JENKINS_LATEST_JOB
+    while [ "$is_building" !=  "false" ]
+    do
+        is_building $JENKINS_LATEST_JOB
+    done
+}
+
+function result {
+    result=`jenkins_job_instance $JENKINS_LATEST_JOB "result" "//result/text()"`
+    echo $result
+}
+
+function evaluate_result {
+    if [ "$result" == "SUCCESS" ]; then
+        exit 0
+    elif [ "$result" == "FAILURE" ]; then
+        exit 1
+    fi
+}
+
+latest_job
+wait_to_complete
+result
+evaluate_result
