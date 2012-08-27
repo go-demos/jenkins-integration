7bb194a85b59fcd374000a6dc7bf0214e7da8630
Pavan Sudarshan <itspanzi@gmail.com>
2012-08-27 14:25:43 +0530

    Removed unused script

--- get_latest_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-JENKINS_HOST=localhost
-JENKINS_PORT=10000
-JENKINS_JOB=website-build
-
-curl "http://$JENKINS_HOST:$JENKINS_PORT/job/$JENKINS_JOB/api/xml?tree=lastBuild\[number\]&xpath=//number/text()" > latest_build
